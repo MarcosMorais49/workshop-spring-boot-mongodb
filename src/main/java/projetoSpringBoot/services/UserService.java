a2e706c5710bb00c85e7735aeabf065fb29bad1c
Marcos Morais <marcosvmorais49@yahoo.com.br>
2024-11-01 20:48:18 -0300

    Obtendo um usuário por id

--- src/main/java/projetoSpringBoot/services/UserService.java
+++ src/main/java/projetoSpringBoot/services/UserService.java
@@ -1,12 +1,14 @@
 package projetoSpringBoot.services;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import projetoSpringBoot.domain.User;
 import projetoSpringBoot.repository.UserRepository;
+import projetoSpringBoot.services.exception.ObjectNotFoundException;
 
 @Service
 public class UserService {
@@ -17,4 +19,11 @@ public class UserService {
 	public List<User> findAll(){
 		return repo.findAll();
 	}
+	
+	public User findById(String id) {
+		Optional<User> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+		}
+	
+	
 }
