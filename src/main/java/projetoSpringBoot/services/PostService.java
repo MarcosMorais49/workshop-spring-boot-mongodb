0006184cb84279fcb82ba2cee29d5ea7a69f9925
Marcos Morais <marcosvmorais49@yahoo.com.br>
2024-11-05 11:23:17 -0300

    obtendo post por id

---
+++ src/main/java/projetoSpringBoot/services/PostService.java
@@ -0,0 +1,22 @@
+package projetoSpringBoot.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import projetoSpringBoot.domain.Post;
+import projetoSpringBoot.repository.PostRepository;
+import projetoSpringBoot.services.exception.ObjectNotFoundException;
+
+@Service
+public class PostService {
+	
+	@Autowired
+	private PostRepository repo;
+
+	public Post findById(String id) {
+		Optional<Post> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+		}
+}
